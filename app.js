3ef01fbedfdafd7ff2f56c3f0e1a4d631b2b3924
Sergei Ermilov <ermilov@mail.com>
2020-10-09 00:21:54 +0200

    Made delete button

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// ToDo Fast Javascript
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+// Functions
+function addTodo(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // Todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Creat list elements
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // check button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // delete button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  // append todoDiv to ul
+  todoList.appendChild(todoDiv);
+  // clear todo input value
+  todoInput.value = "";
+}
+function deleteCheck(event) {
+  /* console.log(event.target); */
+  // delete todo
+  const item = event.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+  // check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
